45a5a78c1468c15e38187499f2a1c27be2bf95f2
Adriana <a2020114976@estudiantes.upsa.edu.bo>
2021-10-13 19:51:31 -0400

    Crear la clase Suerte

---
+++ SegundoParcialAdrianaQuiroz/Models/Suerte.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialAdrianaQuiroz.Models
+{
+    public class Suerte
+    {
+        [Key]
+        [Required]
+        public int SuerteId { get; set; }
+        [Required]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La suerte debe tener entre 10 y 200 caracteres")]
+        public string Detalle { get; set; }
+        [Required]
+        [Url]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "El link debe tener entre 10 y 200 caracteres")]
+        public string Imagen { get; set; }
+
+    }
+}
