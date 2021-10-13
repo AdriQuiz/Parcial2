922c5ca9d977b08ffb71bd14bff18bcd5789c9d8
Adriana <a2020114976@estudiantes.upsa.edu.bo>
2021-10-13 19:57:21 -0400

    Crear DbContext y cambiar al Startup

---
+++ SegundoParcialAdrianaQuiroz/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SegundoParcialAdrianaQuiroz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialAdrianaQuiroz.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Suerte> Suerte { get; set; }
+    }
+}
