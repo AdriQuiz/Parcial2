dd93c544253dedd33918878174a526ddf361fbc8
Adriana <a2020114976@estudiantes.upsa.edu.bo>
2021-10-13 20:02:47 -0400

    Migrar modelos

---
+++ SegundoParcialAdrianaQuiroz/Migrations/20211014000110_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SegundoParcialAdrianaQuiroz.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suerte",
+                columns: table => new
+                {
+                    SuerteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Detalle = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suerte", x => x.SuerteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suerte");
+        }
+    }
+}
