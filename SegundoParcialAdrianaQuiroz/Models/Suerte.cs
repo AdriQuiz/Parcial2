787d0629667e733a4f9c34b98dcf821cf199c613
Adriana <a2020114976@estudiantes.upsa.edu.bo>
2021-10-13 20:16:55 -0400

    Clase Suerte

--- SegundoParcialAdrianaQuiroz/Models/Suerte.cs
+++ SegundoParcialAdrianaQuiroz/Models/Suerte.cs
@@ -11,10 +11,10 @@ namespace SegundoParcialAdrianaQuiroz.Models
         [Key]
         [Required]
         public int SuerteId { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Escriba el detalle")]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "La suerte debe tener entre 10 y 200 caracteres")]
         public string Detalle { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ponga el link de la imagen")]
         [Url]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "El link debe tener entre 10 y 200 caracteres")]
         public string Imagen { get; set; }
